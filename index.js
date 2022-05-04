6071ea0ab498196c6e48247649e80edf11b2cd6f
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-05-04 16:59:15 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
+//user: ARMY_WAREHOUSE
+//pass: xvNCe7I5jfwGCH5r
 
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb+srv://ARMY_WAREHOUSE:xvNCe7I5jfwGCH5r@cluster0.0ab2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try{
+        client.connect();
+        const vehicleCollection = client.db("WarVehicles").collection("vehicles");
+        app.get('/allvehicles', async(req, res)=>{
+            const query = {};
+            const cursor = vehicleCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+    
+}
+run().catch(console.dir);
 app.get('/', (req, res)=>{
     res.send('Service is running good!!!');
 });
