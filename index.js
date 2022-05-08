05a002b7a51c93465a22fc41eddc7603c7129fd3
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-05-08 20:16:37 +0600

    env file added and dbuser and pass protected

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const uri =
-  "mongodb+srv://ARMY_WAREHOUSE:xvNCe7I5jfwGCH5r@cluster0.0ab2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//pass: ARMY_WAREHOUSE:xvNCe7I5jfwGCH5r
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ab2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
